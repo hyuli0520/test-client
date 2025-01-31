8002dba1e7bb6d3d2361fa365b1adb7c7bd2f0bf
minhyeok <minhyeogjeon945@gmail.com>
2025-01-31 13:38:36 +0900

    test client

--- test-client/main.cpp
+++ test-client/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "h/socket.h"
+using namespace std;
  
 int main()
 {
+	cppx::native::init(5);
 
+	cppx::socket sock(cppx::protocol::tcp);
+	if (!sock.not_invalid())
+		return -1;
+	if (!sock.connect(cppx::endpoint(cppx::ip_address::any, 7777)))
+		return -1;
+
+	cout << "connected" << endl;
 }
