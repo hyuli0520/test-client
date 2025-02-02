30fc932f73521a5f6a915e23c45e7a7f566764ce
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-02 15:43:32 +0900

    update submodule

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "h/socket.h"
+#include "socket.h"
 using namespace std;
  
 int main()
@@ -9,7 +9,7 @@ int main()
 	cppx::socket sock(cppx::protocol::tcp);
 	if (!sock.not_invalid())
 		return -1;
-	if (!sock.connect(cppx::endpoint(cppx::ip_address::any, 7777)))
+	if (!sock.connect(cppx::endpoint(cppx::ip_address::loopback, 7777)))
 		return -1;
 
 	cout << "connected" << endl;
