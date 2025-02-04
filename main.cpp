5e17b70571d5497a518169c2a5c970d99ca98833
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-04 19:35:22 +0900

    recv error solve

--- main.cpp
+++ main.cpp
@@ -11,17 +11,15 @@ int main()
 		return -1;
 	if (!sock.connect(cppx::endpoint(cppx::ip_address::loopback, 7777)))
 		return -1;
-
+	
 	cout << "connected" << endl;
 
 	char buf[1024] = "hello";
-	auto context = new cppx::context;
-	context->_buffer.insert(context->_buffer.end(), buf, buf + sizeof(buf));
-	auto success = sock.send(context);
+	auto success = sock.send(buf);
 	if (success)
-		cout << "send : " << context->_buffer.data() << endl;
+		cout << "send : " << buf << endl;
 
-	Sleep(1000);
+	Sleep(1000); 
 
 	return 0;
 }
